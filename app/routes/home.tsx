f4e6def4dad46c1f9de09b922d719b80e23517d2
tarun <tarunreddy.b@enterpi.com>
2026-03-09 10:42:51 +0530

    nav bar authorization done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
+import Navbar from "components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,10 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="home">
+      <Navbar />
+      <h1 className="text-3xl text-indigo-700 font-extrabold">Home</h1>
+    </div>
+  );
 }
